bc340b6d34718bfb4b0d660e0f3e88b431a1064a
Mysura <reddy.mysura99@gmail.com>
2017-11-28 00:07:33 +0530

    [Form]:Register initail commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
@@ -14,6 +14,7 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     routing
   ],
   providers: [],
