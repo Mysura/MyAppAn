266b5360536acafd2a62dc61b0261efd487fb55b
Mysura <reddy.mysura99@gmail.com>
2017-11-27 22:49:44 +0530

    [An-1]-Initial commit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,8 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {RegisterComponent} from './register/index';
 
 const appRoutes: Routes = [
-  { path: 'register', component:RegisterComponent}
-
+  { path: 'register', component: RegisterComponent}
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
